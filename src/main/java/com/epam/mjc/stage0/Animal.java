a22a4d0876df54b28f0aa8f01ef434d19cd9a3eb
Myroslav Toporovskyi <mtoporovskyy@gmail.com>
2023-12-22 19:05:12 +0200

    stage0-module6-task1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
-public class Animal {
+ public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String fur = this.hasFur ? "a" : "no";
+        String paws = numberOfPaws == 1 ? "paw" : "paws";
+        return "This animal is mostly " +  this.color + ". It has " + this.numberOfPaws + " " + paws + " and " + fur + " fur.";
+    }
 }
